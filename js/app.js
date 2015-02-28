614bb2980fa680e2062c7b61445ca49ded1ed4ac
Patrick Lindsay <tlindsay@una.edu>
2015-02-28 13:04:27 -0600

    Implemented routing. Other things.

--- js/app.js
+++ js/app.js
@@ -4,10 +4,39 @@ angular.module("traveler", ['ngRoute'])
       $routeProvider.when('/', {
         templateUrl:'partials/home.html'
       });
-      // ...
+	  $routeProvider.when('/bookLodging', {
+        templateUrl:'partials/bookLodging.html'
+      });
+      $routeProvider.when('/connection', {
+        templateUrl:'partials/connection.html'
+      });
+      $routeProvider.when('/connections', {
+        templateUrl:'partials/connections.html'
+      });
+      $routeProvider.when('/editTrip', {
+        templateUrl:'partials/editTrip.html'
+      });
+      $routeProvider.when('/hostHome', {
+        templateUrl:'partials/hostHome.html'
+      });
+      $routeProvider.when('/newTrip', {
+        templateUrl:'partials/newTrip.html'
+      });
+      $routeProvider.when('/newTripInterests', {
+        templateUrl:'partials/newTripInterests.html'
+      });
+      $routeProvider.when('/profile', {
+        templateUrl:'partials/profile.html'
+      });
+      $routeProvider.when('/scheduleActivity', {
+        templateUrl:'partials/scheduleActivity.html'
+      });
+      $routeProvider.when('/travelerHome', {
+        templateUrl:'partials/travelerHome.html'
+      });
   })
   
-.controller("main", function($scope) {
+.controller("main", function($scope, $location) {
 	$scope.view = 'home';
 	$scope.history = [];//array to store the history stack (for use with back button)
 	$scope.subView = 0;//For tabs in pages
@@ -27,7 +56,7 @@ angular.module("traveler", ['ngRoute'])
 	
 	$scope.goTo = function(page) {
 		$scope.history.push($scope.view);
-		$scope.view = page;
+		$location.path('/'+page);
 		$scope.subView = 0;//Reset the tab to the default every time a new page is loaded
 		$scope.alertSuccess = false;//reset success alert
 	};
