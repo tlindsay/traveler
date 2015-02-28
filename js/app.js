e042f5b2297ea686ba267bd2637eb5612794b7f2
Tyler Yasaka <web@tyleryasaka.me>
2015-02-28 12:40:22 -0600

    Changes to be committed:
    	modified:   index.html
    	modified:   js/app.js

--- js/app.js
+++ js/app.js
@@ -1,32 +1,41 @@
-var app = angular.module('traveler', []);
+angular.module("traveler", ['ngRoute'])
 
-app = angular.module('traveler', /*["ngRoute","mobile-angular-ui"]*/)
-				 /*.config(function($routeProvider) {
-					$routeProvider.when('/', {
-						templateUrl: 'partials/home.html'
-						//controller: 'appController'
-					});
-					$routeProvider.when('/profile', {
-						templateUrl: 'partials/profile.html'
-						//controller: 'appController'
-					});
-				 })*/
-				 .controller('appController', [
-					'$scope',
-					'$location',
-					function($scope, $location) {
-						// console.log('hello, angular!');
-					}
-				]);
-
-.controller
-		(
-			'LoginCtrl',
-			[
-				'$scope',
-				'$rootScope',
-				'$location',
-				'$http',
-				'APIURL',
-				function($scope, $rootScope, $location, $http, APIURL)
-				{
+.config(function($routeProvider) {
+      $routeProvider.when('/', {
+        templateUrl:'partials/home.html'
+      });
+      // ...
+  })
+  
+.controller("main", function($scope) {
+	$scope.view = 'home';
+	$scope.history = [];//array to store the history stack (for use with back button)
+	$scope.subView = 0;//For tabs in pages
+	$scope.images = [0,1,2,3,4,5,6,7,8,9];//just some dummy values for connection images
+	$scope.alertSuccess = false;
+	
+	$scope.profile = {};
+	$scope.profile.name = "John Smith";
+	$scope.profile.hometown = "Florence, Alabama";
+	$scope.profile.bio = "Hello world!";
+	
+	$scope.newTrip = {};
+	$scope.newTrip.location = {city:"New York",state:"New York",country:"United States"};
+	$scope.selectedTrip = '';//variable to store selected trip
+	
+	$scope.trips = ['Tokyo, Japan','Madrid, Spain','Kuala Lumpur, Malaysia'];//sample trips the user has
+	
+	$scope.goTo = function(page) {
+		$scope.history.push($scope.view);
+		$scope.view = page;
+		$scope.subView = 0;//Reset the tab to the default every time a new page is loaded
+		$scope.alertSuccess = false;//reset success alert
+	};
+	
+	$scope.back = function(){
+		if($scope.history.length > 0) {
+			$scope.view = $scope.history.pop();
+			$scope.subView = 0;
+		}
+	}
+});
