24da492ef2ae94fd71455190d5b1ebce27211288
Tyler Yasaka <web@tyleryasaka.me>
2015-02-28 05:49:37 -0600

    Setting up the general layout.
     Changes to be committed:
    
    	modified:   css/styles.css
    	new file:   fonts/icomoon.eot
    	new file:   fonts/icomoon.svg
    	new file:   fonts/icomoon.ttf
    	new file:   fonts/icomoon.woff
    	new file:   icons.css
    	new file:   images/profile.png
    	modified:   index.html
    	modified:   js/app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
 angular.module("traveler", []).controller("main", function($scope, $http) {
 	$scope.view = 'home';
+	$scope.history = [];//array to store the history stack (for use with back button)
+	$scope.subView = 0;//For tabs in pages
+	$scope.images = [0,1,2,3,4,5,6,7,8,9];//just some dummy values for connection images
+	$scope.trip = '';//variable to store selected trip
+	
 	$scope.goTo = function(page) {
+		$scope.history.push($scope.view);
 		$scope.view = page;
+		$scope.subView = 0;//Reset the tab to the default every time a new page is loaded
 	};
+	
+	$scope.back = function(){
+		if($scope.history.length > 0) {
+			$scope.view = $scope.history.pop();
+			$scope.subView = 0;
+		}
+	}
 });
