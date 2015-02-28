e7be96fb861caa9930382e6a44150748cf2af03f
Patrick Lindsay <tlindsay@una.edu>
2015-02-28 11:59:16 -0600

    Shit be broken

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-angular.module('traveler', ["mobile-angular-ui"]);
+var app = angular.module('traveler', ["ngRoute","mobile-angular-ui"])
+				 .config(function($routeProvider) {
+					$routeProvider.when('/', {
+						templateUrl: 'partials/home.html'
+						//controller: 'appController'
+					});
+					$routeProvider.when('/profile', {
+						templateUrl: 'partials/profile.html'
+						//controller: 'appController'
+					});
+				 }).controller('appController', [
+					'$scope',
+					'$location',
+					function($scope, $location) {
+						// console.log('hello, angular!');
+					}
+				]);
