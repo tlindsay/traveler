ea29cc04c703d99b9bea39caea081252634b2e1d
Patrick Lindsay <tlindsay@una.edu>
2015-02-28 11:28:07 -0600

    Put bower_components back into git. Switched to mobile-angular-ui.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,1 @@
-angular.module('mobile-angular-ui', [
-  'mobile-angular-ui.core.activeLinks',       /* adds .active class to current links */
-  'mobile-angular-ui.core.fastclick',         /* polyfills overflow: auto */
-  'mobile-angular-ui.core.sharedState',       /* SharedState service and directives */
-  'mobile-angular-ui.core.outerClick',        /* outerClick directives */
-  'mobile-angular-ui.components.modals',            /* modals and overlays */
-  'mobile-angular-ui.components.switch',            /* switch form input */
-  'mobile-angular-ui.components.sidebars',          /* sidebars */
-  'mobile-angular-ui.components.scrollable',        /* uiScrollable directives */
-  'mobile-angular-ui.components.capture',           /* uiYieldTo and uiContentFor directives */
-  'mobile-angular-ui.components.navbars'            /* navbars */
-]);
+angular.module('traveler', ["mobile-angular-ui"]);
