777a653cde0178498814dc5f919451c7bc93b3ac
Tyler Yasaka <web@tyleryasaka.me>
2015-02-28 13:33:05 -0600

    Changes to be committed:

--- js/app.js
+++ js/app.js
@@ -4,39 +4,10 @@ angular.module("traveler", ['ngRoute'])
       $routeProvider.when('/', {
         templateUrl:'partials/home.html'
       });
-	  $routeProvider.when('/bookLodging', {
-        templateUrl:'partials/bookLodging.html'
-      });
-      $routeProvider.when('/connection', {
-        templateUrl:'partials/connection.html'
-      });
-      $routeProvider.when('/connections', {
-        templateUrl:'partials/connections.html'
-      });
-      $routeProvider.when('/editTrip', {
-        templateUrl:'partials/editTrip.html'
-      });
-      $routeProvider.when('/hostHome', {
-        templateUrl:'partials/hostHome.html'
-      });
-      $routeProvider.when('/newTrip', {
-        templateUrl:'partials/newTrip.html'
-      });
-      $routeProvider.when('/newTripInterests', {
-        templateUrl:'partials/newTripInterests.html'
-      });
-      $routeProvider.when('/profile', {
-        templateUrl:'partials/profile.html'
-      });
-      $routeProvider.when('/scheduleActivity', {
-        templateUrl:'partials/scheduleActivity.html'
-      });
-      $routeProvider.when('/travelerHome', {
-        templateUrl:'partials/travelerHome.html'
-      });
+      // ...
   })
   
-.controller("main", function($scope, $location) {
+.controller("main", function($scope) {
 	$scope.view = 'home';
 	$scope.history = [];//array to store the history stack (for use with back button)
 	$scope.subView = 0;//For tabs in pages
@@ -55,17 +26,13 @@ angular.module("traveler", ['ngRoute'])
 	$scope.trips = ['Tokyo, Japan','Madrid, Spain','Kuala Lumpur, Malaysia'];//sample trips the user has
 	
 	$scope.goTo = function(page) {
-		if(page == 'home') {
-			page = '';
-		}
-		
 		$scope.history.push($scope.view);
-		$location.path('/'+page);
+		$scope.view = page;
 		$scope.subView = 0;//Reset the tab to the default every time a new page is loaded
 		$scope.alertSuccess = false;//reset success alert
 	};
 	
-	$scope.back = function(){
+	$scope.goBack = function(){
 		if($scope.history.length > 0) {
 			$scope.view = $scope.history.pop();
 			$scope.subView = 0;
