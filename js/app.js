9d201db6464a55edab5fbac12be22c0becc00f01
Tyler Yasaka <web@tyleryasaka.me>
2015-02-28 09:44:11 -0600

    On branch master  Changes to be committed:
    	modified:   css/styles.css
    	new file:   images/map.jpg
    	modified:   index.html
    	modified:   js/app.js

--- js/app.js
+++ js/app.js
@@ -3,12 +3,24 @@ angular.module("traveler", []).controller("main", function($scope, $http) {
 	$scope.history = [];//array to store the history stack (for use with back button)
 	$scope.subView = 0;//For tabs in pages
 	$scope.images = [0,1,2,3,4,5,6,7,8,9];//just some dummy values for connection images
-	$scope.trip = '';//variable to store selected trip
+	$scope.alertSuccess = false;
+	
+	$scope.profile = {};
+	$scope.profile.name = "John Smith";
+	$scope.profile.hometown = "Florence, Alabama";
+	$scope.profile.bio = "Hello world!";
+	
+	$scope.newTrip = {};
+	$scope.newTrip.location = {city:"New York",state:"New York",country:"United States"};
+	$scope.selectedTrip = '';//variable to store selected trip
+	
+	$scope.trips = ['Tokyo, Japan','Madrid, Spain','Kuala Lumpur, Malaysia'];//sample trips the user has
 	
 	$scope.goTo = function(page) {
 		$scope.history.push($scope.view);
 		$scope.view = page;
 		$scope.subView = 0;//Reset the tab to the default every time a new page is loaded
+		$scope.alertSuccess = false;//reset success alert
 	};
 	
 	$scope.back = function(){
